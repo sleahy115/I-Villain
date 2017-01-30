6fbf04be7791f75669cfe0226683fdbf219cbc21
Andrew Devlin and Dan Lauby <dan@>
2017-01-30 12:03:34 -0800

    Finish Villain objects, create hermione, mrIncredible, glenda and batman Hero objects

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,11 @@ var skeletor = new Villain('Skeletor', '../img/skelator.jpg' 4, 2, 1, 'dominate'
 var sauron = new Villain('Sauron', '../img/the-eye-of-sauron.jpg' 5, 1, 5, 'dominate', 'convert', 'body', 'supernatural', 'chunky-monkey');
 var terminator = new Villain('Terminator', '../img/terminator.jpg', 5, 4, 1, 'dominate', 'kill', 'body', 'mechanical', 'rocky-road');
 var gru = new Villain('Gru', '../img/Gru.jpg', 2, 2, 5, 'dominate', 'convert', 'black', 'mechanical', 'chocolate');
-var goblinKing = new Villain('Goblin King', '../img/goblin king.jpg', 'dominate', 'convert', 'cape', 'suernatural', 'pistachio');
+var goblinKing = new Villain('Goblin King', '../img/goblin-king.jpg', 'dominate', 'convert', 'cape', 'suernatural', 'pistachio');
 var khan = new Villain('Khan', '../img/khan.jpeg', 'dominate', 'convert', 'body', 'physical', 'strawberry-sorbet');
 var loki = new Villain('Loki', '../img/loki.png', 2, 3, 5, 'chaos', 'kill', 'helmet', 'supernatural', 'cherry-garcia');
+
+var hermione = new Hero('Hermione', '../img/hermione.jpg', 2, 4, 5);
+var mrIncredible = new Hero('Mr. Incredible', '../img/mr-incredible.jpg', 5, 4, 2);
+var glenda = new Hero('Glenda the Good Witch', '../img/glinda-the-good-witch.jpg', 2, 4, 4);
+var batman = new Hero('Batman', '../img/batman.jpg', 4, 3, 4);
