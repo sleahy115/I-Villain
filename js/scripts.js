96c09cf5c3965a2fe54f88e3fd271676c27940be
Andrew Devlin and Dan Lauby <andrew@>
2017-01-30 10:36:18 -0800

    Create villain and hero constructors

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Villain constructor
+function Villain(name, img, motive, strength, speed, wits, victory, costume, powers, iceCream) {
+  this.villainName = name;
+  this.img = img;
+  this.motive = motive;
+  this.strength = strength;
+  this.speed = speed;
+  this.wits = wits;
+  this.victory = victory;
+  this.costume = costume;
+  this.powers = powers;
+  this.iceCream = iceCream;
+}
+
+// Hero constructor
+function Hero(name, img, strength, speed, wits) {
+  this.heroName = name;
+  this.img = img;
+  this.strength = strength;
+  this.speed = speed;
+  this.wits = wits;
+}
