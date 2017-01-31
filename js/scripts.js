a7d69818159a3ce99450a68898e73313a20dcf8f
Charles Peden and Sarah Leahy <sarah@>
2017-01-31 10:50:36 -0800

    take user input and output villain profile

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,41 @@ function Hero(name, img, strength, speed, wits) {
 //   this.villain = villain;
 //   this.hero = hero;
 // }
+var answersArray = [];
+
+var tendencyTest = function(){
+  var highTendency = 0;
+  var villainTendency = "";
+
+  for (var i = 0; i < villainsArray.length; i++){
+    var tempTendency = 0;
+    if (answersArray[0] === villainsArray[i].motive){
+      tempTendency ++
+    };
+    if (answersArray[1] === villainsArray[i].victory){
+      tempTendency ++
+    };
+    if (answersArray[2] === villainsArray[i].costume){
+      tempTendency ++
+    };
+    if (answersArray[3] === villainsArray[i].powers){
+      tempTendency ++
+    };
+    if (answersArray[4] === villainsArray[i].iceCream){
+      tempTendency ++
+    };
+    if (tempTendency > highTendency){
+      villainTendency = villainsArray[i].villainName;
+      highTendency = tempTendency;
+    };
+  };
+  for(var j = 0; j<villainsArray.length; j++){
+    if (villainTendency === villainsArray[j].villainName){
+      return villainsArray[j];
+    };
+  };
+};
+
 
 
 var darthVader = new Villain('Darth Vader', 'img/darth-vader.jpg', 4, 2, 3, 'dominate', 'convert', 'helmet', 'supernatural', 'lactose-intolerance');
@@ -39,6 +74,9 @@ var gru = new Villain('Gru', 'img/Gru.jpg', 2, 2, 5, 'dominate', 'convert', 'bla
 var goblinKing = new Villain('Goblin King', 'img/goblin-king.jpg', 3, 3, 4, 'dominate', 'convert', 'cape', 'suernatural', 'pistachio');
 var khan = new Villain('Khan', 'img/khan.jpeg', 4, 4, 4, 'destroy', 'convert', 'body', 'physical', 'strawberry-sorbet');
 var loki = new Villain('Loki', 'img/loki.png', 2, 3, 5, 'destroy', 'kill', 'helmet', 'supernatural', 'cherry-garcia');
+
+var villainsArray = [];
+villainsArray.push(darthVader, alienQueen, bellatrix, whiteWitch, skeletor, sauron, terminator, gru, goblinKing, khan, loki)
 
 var hermione = new Hero('Hermione', 'img/hermione.jpg', 2, 4, 5);
 var mrIncredible = new Hero('Mr. Incredible', 'img/mr-incredible.jpg', 5, 4, 2);
@@ -82,6 +120,8 @@ Hero.prototype.displayHero = function() {
   $(".display-hero").append(battle);
 }
 
+
+
 $(function(){
   $("form#villain-questions").submit(function(event){
     event.preventDefault();
@@ -90,73 +130,12 @@ $(function(){
   var costumeInput = $("#costume").val();
   var powersInput = $("#powers").val();
   var iceCreamInput = $("#iceCream").val();
-  console.log(motiveInput);
-
-  var villainsArray = []
-  villainsArray.push(darthVader, alienQueen, bellatrix, whiteWitch, skeletor, sauron, terminator, gru, goblinKing, khan, loki)
-
-  var answersArray = [];
-  var tendencyArray = [];
   answersArray.push(motiveInput, victoryInput, costumeInput, powersInput, iceCreamInput);
-
-  console.log(answersArray);
-
-var tendencyTest = function(){
-  var highTendency = 0;
-  var villainTendency = "";
-
-  for (var i = 0; i < villainsArray.length; i++){
-    var tempTendency = 0;
-    //motive
-    console.log("villainArrayivillainName" + villainsArray[i].villainName)
-    if (answersArray[0] === villainsArray[i].motive){
-      console.log("motive match");
-      tempTendency ++
-      // console.log(tempTendency)
-      // tendencyArray.push(villainsArray[i].villainName);
-      // console.log(tendencyArray);
-    };
-    //victory
-    if (answersArray[1] === villainsArray[i].victory){
-      console.log("victory match")
-      tempTendency ++
-      // tendencyArray.push(villainsArray[i].villainName);
-      // console.log("victory match");
-    };
-    //costume
-    if (answersArray[2] === villainsArray[i].costume){
-      console.log("costume match")
-      tempTendency ++
-      // tendencyArray.push(villainsArray[i].villainName);
-      // console.log("costume match");
-    };
-    //powers
-    if (answersArray[3] === villainsArray[i].powers){
-      console.log("powers match")
-      tempTendency ++
-      // tendencyArray.push(villainsArray[i].villainName);
-      // console.log("powers match");
-    };
-    //iceCream
-    if (answersArray[4] === villainsArray[i].iceCream){
-      console.log("icecream match")
-      tempTendency ++
-      // tendencyArray.push(villainsArray[i].villainName);
-      // console.log("icecream match");
-    };
-    if (tempTendency > highTendency){
-      villainTendency = villainsArray[i].villainName;
-      highTendency = tempTendency;
-      console.log(villainsArray[i].villainName)
-      console.log("villain set to " + villainTendency)
-    }
-  };
-  console.log("villainTendency is " + villainTendency)
-  return villainTendency;
-};
-
-var villainOutput =tendencyTest();
-console.log(villainOutput);
+  var villainOutput = tendencyTest();
+  console.log([villainOutput]);
+  $("#stage-one").hide();
+  $("#stage-two").show();
+  villainOutput.villainProfile();
 
   });
 });
