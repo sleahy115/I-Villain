d27be6de672d5c3a7b59566bd07df04b2c48ab15
Andrew Devlin and Dan Lauby <dan@>
2017-01-31 09:15:03 -0800

    Create a prototype to display villain and hero in VS.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // Villain constructor
-
 function Villain(name, img, strength, speed, wits, motive, victory, costume, powers, iceCream) {
   this.villainName = name;
   this.img = img;
@@ -21,11 +20,6 @@ function Hero(name, img, strength, speed, wits) {
   this.speed = speed;
   this.wits = wits;
 }
-
-// function Battle(villain, hero) {
-//   this.villain = villain;
-//   this.hero = hero;
-// }
 
 
 var darthVader = new Villain('Darth Vader', 'img/darth-vader.jpg', 4, 2, 3, 'dominate', 'convert', 'helmet', 'supernatural', 'lactose-intolerance');
@@ -51,7 +45,7 @@ var superMario = new Hero('Super Mario', 'img/Super-Mario.jpg', 4, 4, 2);
 var docBrown = new Hero('Doc Brown', 'img/doc-brown.jpg', 2, 5, 5);
 var professorX = new Hero('Professor X', 'img/prof-x.jpg', 5, 1, 5);
 
-var newBattle = new Battle(darthVader, hermione);
+
 // User Logic
 Villain.prototype.villainProfile = function() {
   var profile = '';
@@ -68,19 +62,28 @@ Villain.prototype.villainProfile = function() {
   $(".villain-profile").append(profile);
 }
 
-// Villain.prototype.displayVillain = function() {
-//   var battle = '';
-//   battle += "<img src='" + this.villain.img + "' alt='image of '" + this.villainName + "'";
-//   battle += "<h2>" + this.villainName + "</h2>";
-//   $(".display-villain").append(battle);
-// }
-//
-// Hero.prototype.displayHero = function() {
-//   var battle = '';
-//   battle += "<img src='" + this.hero.img + "' alt='image of '" + this.heroName + "'";
-//   battle += "<h2>" + this.heroName + "</h2>";
-//   $(".display-hero").append(battle);
-// }
+Villain.prototype.villainDisplay = function() {
+  var profile = '';
+  profile += "<img src='" + this.img + "' alt='image of '" + this.villainName + "'";
+  profile += "<h1>";
+  profile += this.villainName;
+  profile += "</h1>";
+  $(".display-villain").append(profile);
+}
+
+Hero.prototype.heroDisplay = function() {
+  var profile = '';
+  profile += "<img src='" + this.img + "' alt='image of '" + this.heroName + "'";
+  profile += "<h1>";
+  profile += this.heroName;
+  profile += "</h1>";
+  $(".display-hero").append(profile);
+}
+
+
+
+
+
 
 // $(function(){
 //   $("form#villain-questions").submit(function(event){
@@ -106,4 +109,3 @@ Villain.prototype.villainProfile = function() {
 //
 //   });
 // });
-
