c2e9b9b9d309ecbf1129807f093480dcdbcf89cb
Andrew Devlin and Dan Lauby <andrew@>
2017-01-31 11:12:15 -0800

    resize images

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ function Hero(name, img, strength, speed, wits) {
 var darthVader = new Villain('Darth Vader', 'img/darth-vader.jpg', 4, 2, 3, 'dominate', 'convert', 'helmet', 'supernatural', 'lactose-intolerance');
 var alienQueen = new Villain('Alien Queen', 'img/alien.jpg', 5, 4, 1, 'dominate', 'kill', 'body', 'physical', 'strawberry');
 var bellatrix = new Villain('Bellatrix LeStrange', 'img/bellatrix-the-strange.jpg', 3, 4, 3, 'chaos', 'torture', 'cape', 'supernatural', 'mint-chip');
-var whiteWitch = new Villain('White Witch', 'img/bellatrix-the-strange.jpg', 4, 2, 4, 'dominate', 'kill', 'cape', 'supernatural', 'vanilla');
+var whiteWitch = new Villain('White Witch', 'img/whitewitch.jpg', 4, 2, 4, 'dominate', 'kill', 'cape', 'supernatural', 'vanilla');
 var skeletor = new Villain('Skeletor', 'img/skelator.jpg', 4, 2, 1, 'dominate', 'torture', 'underwear', 'supernatural', 'bubble-gum');
 var sauron = new Villain('Sauron', 'img/the-eye-of-sauron.jpg', 5, 1, 5, 'dominate', 'convert', 'body', 'supernatural', 'chunky-monkey');
 var terminator = new Villain('Terminator', 'img/terminator.jpg', 5, 4, 1, 'dominate', 'kill', 'body', 'mechanical', 'rocky-road');
@@ -39,7 +39,7 @@ var mrIncredible = new Hero('Mr. Incredible', 'img/mr-incredible.jpg', 5, 4, 2);
 var glenda = new Hero('Glenda the Good Witch', 'img/glinda-the-good-witch.jpg', 2, 4, 4);
 var batman = new Hero('Batman', 'img/batman.jpg', 4, 3, 4);
 var daenerys = new Hero('Daenerys', 'img/daenerys.jpg', 2, 2, 4);
-var roadRunner = new Hero('Road Runner', 'img/roadrunner.gif', 1, 5, 4);
+var roadRunner = new Hero('Road Runner', 'img/roadrunner.png', 1, 5, 4);
 var dudleyDoRight = new Hero('Dudley Do Right', 'img/dudley-do-right.jpg', 3, 4, 1);
 var superMario = new Hero('Super Mario', 'img/Super-Mario.jpg', 4, 4, 2);
 var docBrown = new Hero('Doc Brown', 'img/doc-brown.jpg', 2, 5, 5);
